f842c4d7d09611e24f8bae6e1c8f2a5d5ff9db5c
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-29 13:24:23 -0300

    feat: integrate register page with api

--- src/view/pages/Register/useRegisterController.ts
+++ src/view/pages/Register/useRegisterController.ts
@@ -1,6 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { authService } from '../../../app/services/authService';
 
 const schema = z.object({
   name: z.string().min(1, 'Nome é obrigatório'),
@@ -25,8 +26,9 @@ export function useRegisterController() {
     resolver: zodResolver(schema),
   });
 
-  const handleSubmit = hookFormHandleSubmit((data) => {
-    console.log(data);
+  const handleSubmit = hookFormHandleSubmit(async (data) => {
+    const { accessToken } = await authService.signup(data);
+    console.log(accessToken);
   });
 
   return { handleSubmit, register, errors };
