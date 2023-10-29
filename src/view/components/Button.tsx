78d9f08d757613cd19a3feb0297e2ea9b7e6eaec
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-29 14:32:41 -0300

    feat: add spinner to Button component

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,5 +1,6 @@
 import { ComponentProps } from 'react';
 import { cn } from '../../app/utils/cn';
+import { Spinner } from './Spinner';
 
 interface ButtonProps extends ComponentProps<'button'> {
   isLoading?: boolean;
@@ -9,6 +10,7 @@ export function Button({
   className,
   isLoading,
   disabled,
+  children,
   ...props
 }: ButtonProps) {
   return (
@@ -16,9 +18,12 @@ export function Button({
       {...props}
       disabled={disabled || isLoading}
       className={cn(
-        'bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl font-medium text-white disabled:text-gray-400 disabled:cursor-not-allowed transition-all tracking-[-0.5px]',
+        'bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl font-medium text-white disabled:text-gray-400 disabled:cursor-not-allowed transition-all tracking-[-0.5px] flex items-center justify-center',
         className,
       )}
-    ></button>
+    >
+      {isLoading && <Spinner className="w-6 h-6" />}
+      {!isLoading && children}
+    </button>
   );
 }
