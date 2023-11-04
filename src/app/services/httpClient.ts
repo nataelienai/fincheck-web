06a72aded4bf7f155dae60f85f9166095c64be64
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-11-04 07:54:44 -0300

    refactor: add delay to http requests for testing

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { localStorageKeys } from '../config/localStorageKeys';
+import { sleep } from '../utils/sleep';
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -13,4 +14,10 @@ httpClient.interceptors.request.use((config) => {
   }
 
   return config;
+});
+
+httpClient.interceptors.response.use(async (data) => {
+  await sleep(1000);
+
+  return data;
 });
