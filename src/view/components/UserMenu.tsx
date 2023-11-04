de56596cd76db1c581de9c847b872505c37fcebc
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-11-04 08:14:00 -0300

    fix: make drop down menu trigger a button

--- src/view/components/UserMenu.tsx
+++ src/view/components/UserMenu.tsx
@@ -8,11 +8,11 @@ export function UserMenu() {
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <div className="h-12 w-12 bg-teal-50 rounded-full flex items-center justify-center">
+        <button className="h-12 w-12 bg-teal-50 rounded-full flex items-center justify-center">
           <span className="text-sm leading-none tracking-[-0.5px] text-teal-900 font-medium">
             NA
           </span>
-        </div>
+        </button>
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Content className="w-32">
