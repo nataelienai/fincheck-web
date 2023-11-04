b3e13c105f4c2a5f3fbd398d15cbdec4f2bd762e
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-11-04 08:15:25 -0300

    fix: increase dahsboard FAB z-index

--- src/view/pages/Dashboard/components/Fab/index.tsx
+++ src/view/pages/Dashboard/components/Fab/index.tsx
@@ -5,7 +5,7 @@ import { CategoryIcon } from '../../../../components/icons/categories/CategoryIc
 
 export function Fab() {
   return (
-    <div className="fixed right-4 bottom-4">
+    <div className="fixed right-4 bottom-4 z-20">
       <DropdownMenu.Root>
         <DropdownMenu.Trigger>
           <button className="text-white w-12 h-12 bg-teal-900 rounded-full flex items-center justify-center">
