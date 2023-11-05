43cc819ed6e0c75736525d3b59c8a087336db3c1
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-11-05 17:42:28 -0300

    fix: invalidate bank accounts query after transaction mutations
    Necessary to reload bank accounts' balance

--- src/view/pages/Dashboard/modals/EditTransactionModal/useEditTransactionModalController.ts
+++ src/view/pages/Dashboard/modals/EditTransactionModal/useEditTransactionModalController.ts
@@ -71,6 +71,7 @@ export function useEditTransactionModalController(
       });
 
       queryClient.invalidateQueries({ queryKey: ['transactions'] });
+      queryClient.invalidateQueries({ queryKey: ['bankAccounts'] });
       toast.success(
         transaction.type === 'EXPENSE'
           ? 'Despesa editada com sucesso!'
@@ -91,6 +92,7 @@ export function useEditTransactionModalController(
       await removeTransaction(transaction.id);
 
       queryClient.invalidateQueries({ queryKey: ['transactions'] });
+      queryClient.invalidateQueries({ queryKey: ['bankAccounts'] });
       toast.success(
         transaction.type === 'EXPENSE'
           ? 'Despesa excluída com sucesso!'
